3ea735eea4445043dbbcdf9da74865730e2e80f2
knoddin <knoddin@gmail.com>
2016-11-19 17:29:33 -0500

    added crud scripts for all comments all are working

--- scripts/comments/edit-comment.sh
+++ scripts/comments/edit-comment.sh
@@ -1,9 +1,9 @@
-curl --include --request POST http://localhost:4741/comments \
+curl --include --request PATCH http://localhost:4741/comments/2 \
   --header "Authorization: Token token=BAhJIiVhYjhlNjQyNWE2OWIxY2I5OWE0NDA1ODdlMzhmZWZmMQY6BkVG--96352226f9898e409eb34def9940b3f442678c2e" \
   --header "Content-Type: application/json" \
   --data '{
     "comment": {
-      "body": "This is a new test comment.",
-      "rating": 4
+      "body": "this is an edited comment",
+      "rating": 3
     }
   }'
