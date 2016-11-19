67ac5f211307ba409b30a4100fabc4668ce631a8
knoddin <knoddin@gmail.com>
2016-11-19 16:51:35 -0500

    removed editable methods from tags controller

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,5 +1,5 @@
 class TagsController < ApplicationController
-  before_action :set_tag, only: [:show, :update, :destroy]
+  before_action :set_tag, only: [:show]
 
   # GET /tags
   # GET /tags.json
@@ -13,38 +13,6 @@ class TagsController < ApplicationController
   # GET /tags/1.json
   def show
     render json: @tag
-  end
-
-  # POST /tags
-  # POST /tags.json
-  def create
-    @tag = Tag.new(tag_params)
-
-    if @tag.save
-      render json: @tag, status: :created, location: @tag
-    else
-      render json: @tag.errors, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH/PUT /tags/1
-  # PATCH/PUT /tags/1.json
-  def update
-    @tag = Tag.find(params[:id])
-
-    if @tag.update(tag_params)
-      head :no_content
-    else
-      render json: @tag.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /tags/1
-  # DELETE /tags/1.json
-  def destroy
-    @tag.destroy
-
-    head :no_content
   end
 
   private
