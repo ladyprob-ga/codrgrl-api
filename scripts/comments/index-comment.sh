1f3557f2f91361c4a8888a74ed52630f6fe437b7
knoddin <knoddin@gmail.com>
2016-11-19 17:31:43 -0500

    adding index script for comments - it works

---
+++ scripts/comments/index-comment.sh
@@ -0,0 +1,2 @@
+curl --include --request GET http://localhost:4741/comments \
+  --header "Authorization: Token token=BAhJIiVhYjhlNjQyNWE2OWIxY2I5OWE0NDA1ODdlMzhmZWZmMQY6BkVG--96352226f9898e409eb34def9940b3f442678c2e"
